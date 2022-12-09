a6dbaa05681b342dc3cdcb803178cf539751c972
kotatsuyaki <robinhuang123@gmail.com>
2022-12-09 18:08:41 +0800

    Make window listener debug messages obvious

--- src/dev/kotatsu/rcptry2/startup/Startup.java
+++ src/dev/kotatsu/rcptry2/startup/Startup.java
@@ -6,31 +6,29 @@ import org.eclipse.ui.IWorkbenchWindow;
 import org.eclipse.ui.PlatformUI;
 
 public class Startup implements IStartup {
-
 	@Override
 	public void earlyStartup() {
 		System.out.println("Early startup");
 		PlatformUI.getWorkbench().addWindowListener(new IWindowListener() {
 			@Override
 			public void windowActivated(IWorkbenchWindow window) {
-				System.out.println("Window activated");
+				System.out.println("WINDOW LISTENER: Window activated");
 			}
 
 			@Override
 			public void windowDeactivated(IWorkbenchWindow window) {
-				System.out.println("Window de-activated");
+				System.out.println("WINDOW LISTENER: Window de-activated");
 			}
 
 			@Override
 			public void windowClosed(IWorkbenchWindow window) {
-				System.out.println("Window closed");
+				System.out.println("WINDOW LISTENER: Window closed");
 			}
 
 			@Override
 			public void windowOpened(IWorkbenchWindow window) {
-				System.out.println("Window opened");
+				System.out.println("WINDOW LISTENER: Window opened");
 			}
 		});
 	}
-
 }
